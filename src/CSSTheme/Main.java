af6fdc2c1995b13ebd39258b2429e6fa574c57ae
shiva sharma <shivathebravo@hotmail.com>
2017-11-08 18:58:56 -0500

    Confim Box

---
+++ src/CSSTheme/Main.java
@@ -0,0 +1,71 @@
+package CSSTheme;
+
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Scene scene = new Scene(grid, 300, 275);
+
+        Text scenetitle = new Text("Please Login: ");
+        Label userName = new Label("User Name:");
+        userName.setStyle("-fx-text-fill:red");
+        TextField userTextField = new TextField();
+        Label pw = new Label("Password: ");
+        PasswordField pwBox = new PasswordField();
+
+        grid.add(scenetitle, 0, 0, 2, 1);
+        grid.add(userName, 0, 1);
+        grid.add(userTextField, 1, 1);
+        grid.add(pw, 0, 2);
+        grid.add(pwBox, 1, 2);
+
+        grid.setGridLinesVisible(false);
+
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+        btn.setOnAction(event ->
+        {
+
+            actiontarget.setText("Sign in button pressed");
+
+        });
+
+
+        primaryStage.setTitle("JavaFX Login Form");
+        scene.getStylesheets().add(Main.class.getResource("Viper.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
+
