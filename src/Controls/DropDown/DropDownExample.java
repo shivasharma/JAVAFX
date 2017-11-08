7d0c1d5a7ed9524d7877190a837beec2a6826d28
shiva sharma <shivathebravo@hotmail.com>
2017-11-08 18:38:06 -0500

    Confim Box

--- src/Controls/DropDown/DropDownExample.java
+++ src/Controls/DropDown/DropDownExample.java
@@ -5,6 +5,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -13,7 +14,7 @@ public class DropDownExample extends Application {
     Stage window;
     Scene scene;
     Button button;
-
+    Label label;
     public static void main(String[] args) {
         launch(args);
     }
@@ -24,7 +25,7 @@ public class DropDownExample extends Application {
         window = primaryStage;
         window.setTitle("DropDown Demo");
         button = new Button("Click Me");
-
+        label = new Label();
         ChoiceBox<String> choiceBox = new ChoiceBox<>();
 
         //getitems returns the observableList object
@@ -32,16 +33,24 @@ public class DropDownExample extends Application {
         choiceBox.getItems().add("Bananas");
         choiceBox.getItems().addAll("Grapes", "PineApple");
         //set default value
-        choiceBox.setValue("Apple");
+        choiceBox.setValue("Apples");
 
+        button.setOnAction(e -> getChoice(choiceBox));
 
         VBox layout = new VBox(10);
 
         layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(button, choiceBox);
+        layout.getChildren().addAll(button, choiceBox, label);
 
         scene = new Scene(layout, 300, 250);
         window.setScene(scene);
         window.show();
     }
+
+    private void getChoice(ChoiceBox<String> choiceBox) {
+
+        String foo = choiceBox.getValue();
+        label.setText(foo);
+    }
+
 }
